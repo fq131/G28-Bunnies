1e4d616804c6ce73f97a8644793b2f415fb38589
fq <136184888+fq131@users.noreply.github.com>
2024-04-24 00:19:49 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:yumify/screen/navigator.dart';
+import 'package:yumify/screen/splash.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +15,7 @@ void main() async {
   );
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Future.delayed(
-    const Duration(seconds: 2),
+    const Duration(microseconds: 1),
   );
   FlutterNativeSplash.remove();
   runApp(const MyApp());
@@ -30,52 +30,7 @@ class MyApp extends StatelessWidget {
       title: 'Group Practical Assignment',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Color.fromARGB(255, 255, 255, 255)),
-      home: const NavPage(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-
-    _controller.addListener(() {
-      setState(() {});
-    });
-
-    _controller.forward().then((_) {});
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FadeTransition(
-          opacity: _animation,
-          child: Image.asset('assets/logo.jpg'),
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
